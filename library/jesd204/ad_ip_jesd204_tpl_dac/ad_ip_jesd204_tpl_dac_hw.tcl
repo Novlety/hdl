e7847fe84279dd4c77b67b4f9bf175dcc50554c9
Lars-Peter Clausen <lars@metafoo.de>
2018-09-17 14:57:12 +0200

    ad_ip_jesd204_tpl_dac: Use perfect shuffle helper module
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>

--- library/jesd204/ad_ip_jesd204_tpl_dac/ad_ip_jesd204_tpl_dac_hw.tcl
+++ library/jesd204/ad_ip_jesd204_tpl_dac/ad_ip_jesd204_tpl_dac_hw.tcl
@@ -29,12 +29,13 @@ ad_ip_create ad_ip_jesd204_tpl_dac "JESD204 Transport Layer for DACs" p_ad_ip_je
 set_module_property VALIDATION_CALLBACK p_ad_ip_jesd204_tpl_dac_validate
 ad_ip_files ad_ip_jesd204_tpl_dac [list \
   $ad_hdl_dir/library/altera/common/ad_mul.v \
-  $ad_hdl_dir/library/common/ad_dds_sine.v \
-  $ad_hdl_dir/library/common/ad_dds_cordic_pipe.v \
-  $ad_hdl_dir/library/common/ad_dds_sine_cordic.v \
-  $ad_hdl_dir/library/common/ad_dds_2.v \
-  $ad_hdl_dir/library/common/ad_dds_1.v \
   $ad_hdl_dir/library/common/ad_dds.v \
+  $ad_hdl_dir/library/common/ad_dds_1.v \
+  $ad_hdl_dir/library/common/ad_dds_2.v \
+  $ad_hdl_dir/library/common/ad_dds_cordic_pipe.v \
+  $ad_hdl_dir/library/common/ad_dds_sine.v \
+  $ad_hdl_dir/library/common/ad_dds_sine_cordic.v \
+  $ad_hdl_dir/library/common/ad_perfect_shuffle.v \
   $ad_hdl_dir/library/common/ad_rst.v \
   $ad_hdl_dir/library/common/up_axi.v \
   $ad_hdl_dir/library/common/up_xfer_cntrl.v \
